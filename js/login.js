7cca8526ad1a4e06f3df05ee0944f276a0addb5c
mharif1 <md5667676@gmail.com>
2022-08-16 01:06:37 +0600

    email & password section addded

--- js/login.js
+++ js/login.js
@@ -1,3 +1,16 @@
+//step 1:add click even handler with the submit button
 document.getElementById('btn-submit').addEventListener('click',function(){
-    console.log('submit button clicked')
+    //step 2: get the email address inside the email input field
+    // always remember to use .value to get text from as input field 
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    console.log(email)
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value; 
+    console.log(password);
+    //step 3: get password
+    //3.a : set id on the html element
+    //3.b : get the element
+    //3.c : get the value from the element
+
 })

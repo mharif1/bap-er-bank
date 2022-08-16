3029f6fdd99a5006be828dd65693d9672e786094
mharif1 <md5667676@gmail.com>
2022-08-16 13:33:03 +0600

    deposit set

--- js/deposit.js
+++ js/deposit.js
@@ -1,14 +1,25 @@
-// step 1: add even listener to the deposit button 
+// step-1: add even listener to the deposit button 
 document.getElementById('btn-deposit').addEventListener('click',function(){
-    // step 2 : get the deposit amount from the deposit input field 
+    // step-2 : get the deposit amount from the deposit input field 
     // For input field use .value to the value inside the input field
     const depositFileld =document.getElementById('deposit-field');
-    const depositAmount =depositFileld.value ;
-    
-    // step 3 : get the current deposit total 
+    const newDepositAmountString =depositFileld.value ;
+    const newDepositAmount =parseFloat(newDepositAmountString);
+
+    // step-3 : get the current deposit total 
     //for non-input(element other than input, textarea)use .innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
-    console.log(depositAmount)
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString)
+    
+    // step-4 : get number to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // step-5 :get ballance current total
+    
+
+
+    // step 7 : clear the deposit field 
+    depositFileld.value = '';
 })

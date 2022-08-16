de0cb3f5f5905e4fd33ef3f52e54b47919700997
mharif1 <md5667676@gmail.com>
2022-08-16 16:05:50 +0600

    all done

--- js/deposit.js
+++ js/deposit.js
@@ -1,17 +1,17 @@
 // step-1: add even listener to the deposit button 
-document.getElementById('btn-deposit').addEventListener('click',function(){
+document.getElementById('btn-deposit').addEventListener('click', function () {
     // step-2 : get the deposit amount from the deposit input field 
     // For input field use .value to the value inside the input field
-    const depositFileld =document.getElementById('deposit-field');
-    const newDepositAmountString =depositFileld.value ;
-    const newDepositAmount =parseFloat(newDepositAmountString);
+    const depositFileld = document.getElementById('deposit-field');
+    const newDepositAmountString = depositFileld.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
 
     // step-3 : get the current deposit total 
     //for non-input(element other than input, textarea)use .innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
     const previousDepositTotalString = depositTotalElement.innerText;
     const previousDepositTotal = parseFloat(previousDepositTotalString)
-    
+
     // step-4 : get number to set the total deposit
     const currentDepositTotal = previousDepositTotal + newDepositAmount;
     depositTotalElement.innerText = currentDepositTotal;
@@ -21,7 +21,7 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
     const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
-    balanceTotalElement.innerText = currentBalanceTotal
+    balanceTotalElement.innerText = currentBalanceTotal;
     // step 7 : clear the deposit field 
     depositFileld.value = '';
 })
